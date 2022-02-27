6d22073d1c14f05221616224133d36f70829f80e
Serviantx <eduardoas0223@gmail.com>
2022-02-27 18:03:00 -0500

    Subida del API

---
+++ laravel/app/Http/Controllers/EmpleadosController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Empleado;
+use Illuminate\Http\Request;
+
+class EmpleadosController extends Controller
+{
+    function obtenerLista()
+	{
+		$empleados =  Empleado::all();
+
+
+		$response = new \stdClass();
+		$response->success=true;
+		$response->data=$empleados;
+
+		return response()->json($response,200);
+	}
+
+    function obtenerItem($id)
+	{
+		$empleado =  Empleado::find($id);
+
+
+		$response = new \stdClass();
+		$response->success=true;
+		$response->data=$empleado;
+
+		return response()->json($response,200);
+	}
+
+
+	function update(Request $request)
+	{
+		$empleado =  Empleado::find($request->id);
+
+		if($empleado)
+		{
+            $empleado->nombre=$request->nombre;
+            $empleado->apellido_paterno=$request->apellido_paterno;
+            $empleado->apellido_materno=$request->apellido_materno;
+            $empleado->numero_documento_identidad=$request->numero_documento_identidad;
+            $empleado->pais=$request->pais;
+			$empleado->save();
+		}
+
+		$response = new \stdClass();
+		$response->success=true;
+		$response->data=$empleado;
+
+		return response()->json($response,200);
+	}
+
+	/*function patch(Request $request)
+	{
+		$empleado =  Empleado::find($request->id);
+
+		if($empleado)
+		{
+
+			if(isset($request->codigo))
+			$producto->codigo=$request->codigo;
+
+			if(isset($request->nombre))
+			$producto->nombre=$request->nombre;
+
+			$producto->save();
+		}
+
+		$response = new \stdClass();
+		$response->success=true;
+		$response->data=$producto;
+
+		return response()->json($response,200);
+	}*/
+
+
+	function store(Request $request)
+	{
+		$empleado = new Empleado();
+		$empleado->nombre=$request->nombre;
+        $empleado->apellido_paterno=$request->apellido_paterno;
+        $empleado->apellido_materno=$request->apellido_materno;
+        $empleado->numero_documento_identidad=$request->numero_documento_identidad;
+        $empleado->pais=$request->pais;
+		$empleado->save();
+
+		$response = new \stdClass();
+		$response->success=true;
+		$response->data=$empleado;
+
+		return response()->json($response,200);
+	}
+
+	function delete($id)
+	{
+		$response = new \stdClass();
+		$response->success=true;
+		$response_code=200;
+
+
+		$empleado = Empleado::find($id);
+
+		if($empleado)
+		{
+			$empleado->delete();
+			$response->success=true;
+			$response_code=200;
+		}
+		else
+		{
+			$response->error=["El empleado ya ha sido eliminado"];
+			$response->success=false;
+			$response_code=500;
+		}
+
+		return response()->json($response,$response_code);
+
+	}
+}
---
+++ laravel/database/migrations/2022_02_27_154651_create_empleados_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->string("nombre",20);
+            $table->string("apellido_paterno",20);
+            $table->string("apellido_materno",20);
+            $table->string("numero_documento_identidad",20);
+            $table->string("pais",20);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
